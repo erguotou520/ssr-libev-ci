99f12e4ad9033205d1dd82dff9cc6a6b33c10d44
erguotou <erguotou525@gmail.com>
2018-05-17 16:41:57 +0800

    tt

---
+++ build.sh
@@ -0,0 +1,14 @@
+mount c:/mingw /mingw
+mingw-get upgrade "w32api<5.0.2"
+autoreconf -f -i
+cd openssl-1.0.2o
+./config --prefix=~/prebuilt --openssldir=~/prebuilt/openssl
+make && make install
+cd ../pcre-8.42
+./config --prefix=/mingw --disable-shared --disable-cpp --enable-newline-is-anycrlf --enable-utf8 --enable-unicode -properties
+cd ../shadowsocksr-libev
+./config --prefix=~/ss --with-openssl=~/prebuilt --disable-documentation --enable-static
+make && make install
+curl -O https://gist.githubusercontent.com/stefanbuck/ce788fee19ab6eb0b4447a85fc99f447/raw/dbadd7d310ce8446de89c4ffdf1db0b400d0f6c3/upload-github-release-asset.sh
+bash upload-github-release-asset.sh github_api_token=$GITHUB_TOKEN owner=erguotou520 repo=ssr-libev-ci tag=0.0.2 filename=~/ss/bin/ss-local.exe
+unmount /mingw
